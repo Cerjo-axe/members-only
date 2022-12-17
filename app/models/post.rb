2089f28cddac19f5ccf01465649e28470935579a
Cerjo-axe <58487647+Cerjo-axe@users.noreply.github.com>
2022-12-17 18:09:33 -0300

    create posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title presence:true
+  validates :title, presence:true, length: {minimum: 10}
+  validates :body, presence:true, length: {maximum:200}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'newpost', to: 'posts#new', as: 'newpost'
   resources :users
+  resources :posts
   resources :sessions
   # Defines the root path route ("/")
-  root "users#new"
+  root "posts#index"
 end
